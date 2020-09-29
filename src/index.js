3d3630568438f981e4180397540e6b29809079db
Odil Abdulloyev <abdulloyevodil@mail.ru>
2020-09-29 21:12:45 +0500

    feat: solution of typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  return (array && array.length > 0) ? array.reduce((a, b) => Math.min(a, b)) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  return (array && array.length > 0) ? array.reduce((a, b) => Math.max(a, b)) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  return (array && array.length > 0) ? array.reduce((a, x) => a += x, 0) / array.length : 0;
 }
